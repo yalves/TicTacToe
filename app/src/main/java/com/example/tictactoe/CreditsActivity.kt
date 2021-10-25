13e3dfb4db4cbacd594fc2a9e94bb06984b73566
Yan Alves <yalves@hotmail.com>
2021-10-25 16:56:02 -0300

    Adds RecyclerView

--- app/src/main/java/com/example/tictactoe/CreditsActivity.kt
+++ app/src/main/java/com/example/tictactoe/CreditsActivity.kt
@@ -2,10 +2,16 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class CreditsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
+
+        val adapter = CustomAdapter(Array(1){ "Yan Alves" })
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.adapter = adapter
     }
 }
